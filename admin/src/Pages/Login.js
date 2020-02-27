33284bd52a3ccfe8147aa6fd40d758cb5008179c
Len-kahone <1030462746@qq.com>
2020-02-27 22:23:22 +0800

    admin登录

--- admin/src/Pages/Login.js
+++ admin/src/Pages/Login.js
@@ -1,59 +1,75 @@
-import React, { useState } from 'react';
-import 'antd/dist/antd.css';
-import { Card, Input, Icon, Button, Spin } from 'antd';
-import {login} from '../api/'
+import React, { useState } from "react";
+import "antd/dist/antd.css";
+import { Card,message, Input, Icon, Button, Spin } from "antd";
+import { login } from "../api/";
 
-import '../static/css/Login.css';
+import "../static/css/Login.css";
 
-export default function Login(){
-const [userName, setUserName] = useState('');
-const [password, setPassword] = useState('');
-const [isLoading, setIsLoading] = useState(false);
+export default function Login(props) {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const checkLogin = () => {
+  
+    if (!userName) {
+      message.error("用户名不能为空");
+      return false;
+    } else if (!password) {
+      message.error("密码不能为空");
+      return false;
+    }
+    let dataProps = {
+      userName: userName,
+      password: password
+    };
     setIsLoading(true);
-    login({userName:'kahone',password:123}).then(res=>{
-      console.log(res)
-    })
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
+    login(dataProps).then(res => {
+      if (res.code == 0) {
+        console.log(res);
+        setIsLoading(false);
+        props.history.push('/index')
+        console.log(props)
+      } else {
+
+      }
+    });
   };
-    return (
-      <div className="login-div">
-        <Spin tip="Loading..." spinning={isLoading}>
-          <Card
-            title="JSPang Blog  System"
-            bordered={true}
-            style={{ width: 400 }}
-          >
-            <Input
-              id="userName"
-              size="large"
-              placeholder="Enter your userName"
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              onChange={e => {
-                setUserName(e.target.value);
-              }}
-            />
-            <br />
-            <br />
-            <Input.Password
-              id="password"
-              size="large"
-              placeholder="Enter your password"
-              prefix={<Icon type="key" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              onChange={e => {
-                setPassword(e.target.value);
-              }}
-            />
-            <br />
-            <br />
-            <Button type="primary" size="large" block onClick={checkLogin}>
-              {' '}
-              Login in{' '}
-            </Button>
-          </Card>
-        </Spin>
-      </div>
-    );
+  return (
+    <div className="login-div">
+      <Spin tip="Loading..." spinning={isLoading}>
+        <Card
+          title="JSPang Blog  System"
+          bordered={true}
+          style={{ width: 400 }}
+        >
+          <Input
+            id="userName"
+            size="large"
+            placeholder="Enter your userName"
+            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+            onChange={e => {
+              setUserName(e.target.value);
+            }}
+          />
+          <br />
+          <br />
+          <Input.Password
+            id="password"
+            size="large"
+            placeholder="Enter your password"
+            prefix={<Icon type="key" style={{ color: "rgba(0,0,0,.25)" }} />}
+            onChange={e => {
+              setPassword(e.target.value);
+            }}
+          />
+          <br />
+          <br />
+          <Button type="primary" size="large" block onClick={checkLogin}>
+            {" "}
+            Login in{" "}
+          </Button>
+        </Card>
+      </Spin>
+    </div>
+  );
 }
--- service/app/controller/admin/index.js
+++ service/app/controller/admin/index.js
@@ -10,23 +10,25 @@ class Main extends Controler {
   }
   async checkLogin() {
     const { userName, password } = this.ctx.request.body;
-    const sql =
-    'SELECT article.id as id,' +
-    'article.title as title,' +
-    'article.introduce as introduce,' +
-    'article.article_content as article_content,' +
-    'article.addTime as addTime,' +
-    'article.view_count as view_count ,' +
-    '.type.typeName as typeName ' +
-    'FROM article LEFT JOIN type ON article.type_id = type.Id' +
-    ' where article.type_id=' +
-    id;
+    const sql = `SELECT * FROM admin_user WHERE username="${userName}"  AND PASSWORD=${password}`;
+
     const result = await this.app.mysql.query(sql);
-    this.ctx.body = { data: result };
-    this.ctx.body = {
-      msg: '登录成功',
-      code: 0,
-    };
+
+    if (result && result.length > 0) {
+      // 登录成功,进行session缓存
+      const openId = new Date().getTime();
+      this.ctx.session.openId = { openId };
+      this.ctx.body = {
+        msg: '登录成功',
+        code: 0,
+      };
+    } else {
+      this.ctx.body = {
+        msg: '登录失败',
+        code: 1,
+      };
+    }
+
   }
 
 }
